433b44f32bc99e7d32dab6b6b0ba5ee8a90ba39c
kalfen <kalfen@naver.com>
2022-11-10 20:05:25 +0900

    데이터 검증 -2

--- src/main/java/com/toy/api/request/PostCreate.java
+++ src/main/java/com/toy/api/request/PostCreate.java
@@ -10,7 +10,7 @@ import javax.validation.constraints.NotBlank;
 @Getter
 public class PostCreate {
 
-    @NotBlank(message = "타이틀을 입력해주세요.")
+    @NotBlank(message = "타이틀을 입력하세요.")
     private String title;
 
     @NotBlank(message = "컨텐츠를 입력해주세요.")
